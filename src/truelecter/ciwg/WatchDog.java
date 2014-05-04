792f5ad6583967787d8926a51a4d33d473cf3472
TrueLecter <andrew.panassiouk@gmail.com>
2014-05-04 22:21:06 +0300

    Added enchantments watchdog

--- src/truelecter/ciwg/WatchDog.java
+++ src/truelecter/ciwg/WatchDog.java
@@ -1,7 +1,10 @@
 package truelecter.ciwg;
 
+import java.util.Map;
+
 import lib.Attributes;
 
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryEvent;
@@ -10,6 +13,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class WatchDog extends JavaPlugin implements Listener {
 
+	private ItemStack removeNotValidEnchantments(ItemStack item) {
+		Map<Enchantment, Integer> Enchants = item.getEnchantments();
+		Map<Enchantment, Integer> newEnchants = item.getEnchantments();
+		ItemStack newItem = new ItemStack(item.getType());
+		for (Enchantment e: Enchants.keySet()){
+			 if (!e.canEnchantItem(new ItemStack(item.getType()))){
+				 continue;
+			 }
+			 if (!(Enchants.get(e)>e.getMaxLevel())){
+				 continue;
+			 }
+			 newEnchants.put(e, Enchants.get(e));
+		}
+		newItem.setItemMeta(item.getItemMeta());
+		newItem.setDurability(item.getDurability());
+		newItem.setAmount(item.getAmount());
+		newItem.addEnchantments(newEnchants);
+		return newItem;
+	}
+	
 	@EventHandler
 	public void onInventory(InventoryEvent event) {
 		ItemStack[] items = event.getInventory().getContents();
@@ -17,11 +40,13 @@ public class WatchDog extends JavaPlugin implements Listener {
 			if (item != null) {
 				Attributes attr = new Attributes(item);
 				attr.clear();
-				item = attr.getStack();
+				item = removeNotValidEnchantments(attr.getStack());
 			}
 		}
 	}
 
+	
+	
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(this, this);
 	}
